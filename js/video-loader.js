8002a03afe53e50deb24a37da113926e7cc4f6e9
Mihran Sahakyan <mir_sah@mail.ru>
2016-07-29 11:45:03 +0300

    in progress

--- js/video-loader.js
+++ js/video-loader.js
@@ -10,12 +10,80 @@
       var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
-          height: '390',
-          width: '640',
           videoId: 'M7lc1UVf-VE',
           events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
+          },
+          playerVars: {
+            controls: 0,
+            disablekb: 1,
+            showinfo: 0
+          }
+        });
+
+        player1 = new YT.Player('player1', {
+          videoId: 'ZJ4aVllpRH8',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          },
+          playerVars: {
+            controls: 0,
+            disablekb: 1,
+            showinfo: 0
+          }
+        });
+
+        player2 = new YT.Player('player2', {
+          videoId: 'qN2ODFAgSOk',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          },
+          playerVars: {
+            controls: 0,
+            disablekb: 1,
+            showinfo: 0
+          }
+        });
+
+        player3 = new YT.Player('player3', {
+          videoId: 'Uh7RVuqVAds',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          },
+          playerVars: {
+            controls: 0,
+            disablekb: 1,
+            showinfo: 0
+          }
+        });
+
+        player4 = new YT.Player('player4', {
+          videoId: '2hbgtQaBWIg',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          },
+          playerVars: {
+            controls: 0,
+            disablekb: 1,
+            showinfo: 0
+          }
+        });
+
+        player5 = new YT.Player('player5', {
+          videoId: 'scUJbiEmiuc',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange,
+          },
+          playerVars: {
+            controls: 0,
+            disablekb: 1,
+            showinfo: 0
           }
         });
       }
@@ -32,14 +100,39 @@
       function onPlayerStateChange(event) {}
       function stopVideo() {
         player.stopVideo();
+        player1.stopVideo();
+        player2.stopVideo();
+        player3.stopVideo();
+        player4.stopVideo();
+        player5.stopVideo();
       }
 
 
       function onPlayerReady(event) {
   
-  // bind events
-  var playButton = document.getElementById("play-button");
-  playButton.addEventListener("click", function() {
-    player.playVideo();
-  });
+          // bind events
+          var playButton = document.getElementById("play-button");
+            playButton.addEventListener("click", function() {
+            player.playVideo();
+          });
+          var playButton = document.getElementById("play-button1");
+           playButton.addEventListener("click", function() {
+           player1.playVideo();
+          });
+          var playButton = document.getElementById("play-button2");
+           playButton.addEventListener("click", function() {
+           player2.playVideo();
+          });
+          var playButton = document.getElementById("play-button3");
+           playButton.addEventListener("click", function() {
+           player3.playVideo();
+          }); 
+          var playButton = document.getElementById("play-button4");
+           playButton.addEventListener("click", function() {
+           player4.playVideo();
+          }); 
+          var playButton = document.getElementById("play-button5");
+           playButton.addEventListener("click", function() {
+           player5.playVideo();
+          }); 
 }
